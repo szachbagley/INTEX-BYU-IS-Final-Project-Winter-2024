8c8862bfa424c243339d4b2b888d344bb4aac7a5
markjamesthuet <markj2it@gmail.com>
2024-04-09 16:12:12 -0600

    commit looney

---
+++ Intex_Group3_6/Views/MarkThuet/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Intex_Group3_6.Views.MarkThuet
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
