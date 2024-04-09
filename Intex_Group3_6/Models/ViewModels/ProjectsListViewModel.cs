aef265413f14a9c985f9702ec7a3d8ff833acb14
Zach Bagley <szachbagley@gmail.com>
2024-04-09 16:10:55 -0600

    help

---
+++ Intex_Group3_6/Models/ViewModels/ProjectsListViewModel.cs
@@ -0,0 +1,10 @@
+﻿//namespace Intex_Group3_6.Models.ViewModels
+//{
+//    public class ProjectsListViewModel
+//    {
+//        public IQueryable<Project> Projects { get; set;}
+//        public PaginationInfo PaginationInfo { get; set;} = new PaginationInfo();
+
+//        public string? CurrentProjectType { get; set;}
+//    }
+//}
