2fe45f0976496f3437ad01b4e118687b65a8e344
marbar00 <barlockerm@gmail.com>
2024-04-09 10:44:32 -0600

    First commit

---
+++ Intex_Group3_6/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Intex_Group3_6.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
