fd73df43f685dc443e8f01a5401fbde10320e3e4
markjamesthuet <markj2it@gmail.com>
2024-04-11 18:33:26 -0600

    final product push

---
+++ Intex_Group3_6/Components/ProductTypesViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Intex_Group3_6.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace Intex_Group3_6.Components
+{
+    public class ProductTypesViewComponent : ViewComponent
+    {
+        private IDataRepo _dataRepo;
+
+
+        public ProductTypesViewComponent(IDataRepo temp)
+        {
+            _dataRepo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedProductType = RouteData?.Values["productType"];
+            var productTypes = new ProductFilterOptions
+            {
+                PrimaryColors = _dataRepo.Products
+                    .Select(x => x.primaryColor)
+                    .Distinct()
+                    .ToList(),
+                Categories = _dataRepo.Products
+                    .Select(x => x.category1)
+                    .Distinct()
+                    .ToList()
+            };
+
+            return View(productTypes);
+        }
+    }
+}
