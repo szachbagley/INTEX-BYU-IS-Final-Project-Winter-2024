7dba2480f797865e9f9112f1672df7b1c2a2edc3
Zach Bagley <szachbagley@gmail.com>
2024-04-12 02:09:46 -0600

    got em

---
+++ Intex_Group3_6/Models/ViewModels/ProductPageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Intex_Group3_6.Models.ViewModels
+{
+    public class ProductPageViewModel
+    {
+        public IQueryable<Product> Products { get; set;}
+        public PaginationInfo PaginationInfo { get; set; } = new PaginationInfo();
+        public string? CurrentProductType { get; set; }
+    }
+}
